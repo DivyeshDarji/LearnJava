0aa4ca1d0df7b227ca3820829537f4d8e68b574b
DivyeshDarji <darji.divyesh@yahoo.com>
2024-01-01 18:36:36 +0530

    Add Create Number Pattern File in Branch1

--- src/com/basicjava/test/CreateNumberPattern.java
+++ src/com/basicjava/test/CreateNumberPattern.java
@@ -9,15 +9,44 @@ public class CreateNumberPattern {
 
 		System.out.println("Regular Number Pattern");
 		System.out.println();
+		
+		/*
+		  	1 
+			1 2 
+			1 2 3 
+			1 2 3 4 
+			1 2 3 4 5 
+		 */
 
 		// row
 		for (int i = 1; i <= 5; i++) {
-
 			// cols
 			for (int j = 1; j <= i; j++) {
 				System.out.print(j + " ");
 			}
 
+			System.out.println();
+		}
+		
+		
+		System.out.println();	
+		System.out.println("Inverted Regular Number Pattern");
+		System.out.println();
+		
+		/*
+		  	1 
+			1 2 
+			1 2 3 
+			1 2 3 4 
+			1 2 3 4 5 
+		 */
+
+		// row
+		for (int i = 5; i >= 1; i--) {
+			// cols
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
 			System.out.println();
 		}
 	}
